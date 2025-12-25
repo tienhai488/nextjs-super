40f1b8a79e38c861d7577b8f432c60b986fa452b
Le Tien Hai <tienhaile488@gmail.com>
2025-12-25 17:29:06 +0700

    feat: ui login page

---
+++ app/(public)/(auth)/login/login-form.tsx
@@ -0,0 +1,69 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { LoginBody, LoginBodyType } from '@/schemaValidations/auth.schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+export default function LoginForm() {
+  const form = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  return (
+    <Card className='mx-auto max-w-sm'>
+      <CardHeader>
+        <CardTitle className='text-2xl'>Đăng nhập</CardTitle>
+        <CardDescription>Nhập email và mật khẩu của bạn để đăng nhập vào hệ thống</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form className='space-y-2 max-w-[600px] shrink-0 w-full' noValidate>
+            <div className='grid gap-4'>
+              <FormField
+                control={form.control}
+                name='email'
+                render={({ field }) => (
+                  <FormItem>
+                    <div className='grid gap-2'>
+                      <FormLabel htmlFor='email'>Email</FormLabel>
+                      <Input id='email' type='email' placeholder='m@example.com' required {...field} />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='password'
+                render={({ field }) => (
+                  <FormItem>
+                    <div className='grid gap-2'>
+                      <div className='flex items-center'>
+                        <FormLabel htmlFor='password'>Password</FormLabel>
+                      </div>
+                      <Input id='password' type='password' required {...field} />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <Button type='submit' className='w-full'>
+                Đăng nhập
+              </Button>
+              <Button variant='outline' className='w-full' type='button'>
+                Đăng nhập bằng Google
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <Button variant={'outline'} className="cursor-pointer m-2">TienHai</Button>
-  );
-}
