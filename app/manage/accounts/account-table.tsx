4819955eb07fc7c484ab3f783dd4a21a77f8d2ba
Le Tien Hai <tienhaile488@gmail.com>
2026-01-05 22:20:27 +0700

    feat: account list

--- app/manage/accounts/account-table.tsx
+++ app/manage/accounts/account-table.tsx
@@ -43,6 +43,7 @@ import { useSearchParams } from 'next/navigation'
 import AutoPagination from '@/components/auto-pagination'
 import EditEmployee from '@/app/manage/accounts/edit-employee'
 import AddEmployee from '@/app/manage/accounts/add-employee'
+import { useGetAccountList } from '@/queries/useAccount'
 
 type AccountItem = AccountListResType['data'][0]
 
@@ -94,6 +95,7 @@ export const columns: ColumnDef<AccountType>[] = [
   },
   {
     id: 'actions',
+    header: 'Actions',
     enableHiding: false,
     cell: function Actions({ row }) {
       const { setEmployeeIdEdit, setEmployeeDelete } = useContext(AccountTableContext)
@@ -165,7 +167,8 @@ export default function AccountTable() {
   // const params = Object.fromEntries(searchParam.entries())
   const [employeeIdEdit, setEmployeeIdEdit] = useState<number | undefined>()
   const [employeeDelete, setEmployeeDelete] = useState<AccountItem | null>(null)
-  const data: any[] = []
+  const accountListQuery = useGetAccountList()
+  const data: any[] = accountListQuery.data?.payload.data || []
   const [sorting, setSorting] = useState<SortingState>([])
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
@@ -205,7 +208,7 @@ export default function AccountTable() {
   }, [table, pageIndex])
 
   return (
-    <AccountTableContext.Provider value={{ employeeIdEdit, setEmployeeIdEdit, employeeDelete, setEmployeeDelete }}>
+    <AccountTableContext value={{ employeeIdEdit, setEmployeeIdEdit, employeeDelete, setEmployeeDelete }}>
       <div className='w-full'>
         <EditEmployee id={employeeIdEdit} setId={setEmployeeIdEdit} onSubmitSuccess={() => {}} />
         <AlertDialogDeleteAccount employeeDelete={employeeDelete} setEmployeeDelete={setEmployeeDelete} />
@@ -268,6 +271,6 @@ export default function AccountTable() {
           </div>
         </div>
       </div>
-    </AccountTableContext.Provider>
+    </AccountTableContext>
   )
 }
