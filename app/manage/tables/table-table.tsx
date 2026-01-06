47a95c559746cb227c0cff3401018a33f02acdfb
Le Tien Hai <tienhaile488@gmail.com>
2026-01-06 16:57:59 +0700

    feat: update filter for table

--- app/manage/tables/table-table.tsx
+++ app/manage/tables/table-table.tsx
@@ -65,7 +65,8 @@ export const columns: ColumnDef<TableItem>[] = [
   {
     accessorKey: 'number',
     header: 'Số bàn',
-    cell: ({ row }) => <div className='capitalize'>{row.getValue('number')}</div>
+    cell: ({ row }) => <div className='capitalize'>{row.getValue('number')}</div>,
+    filterFn: 'includesString'
   },
   {
     accessorKey: 'capacity',
