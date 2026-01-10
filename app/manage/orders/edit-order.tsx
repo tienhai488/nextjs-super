c9a6e14708e195de6ccd6bb19ed0398e04b3540e
Le Tien Hai <tienhaile488@gmail.com>
2026-01-10 17:47:44 +0700

    feat: update order

--- app/manage/orders/edit-order.tsx
+++ app/manage/orders/edit-order.tsx
@@ -7,52 +7,15 @@ import { UpdateOrderBody, UpdateOrderBodyType } from '@/schemaValidations/order.
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { getVietnameseOrderStatus } from '@/lib/utils'
+import { getVietnameseOrderStatus, handleErrorApi } from '@/lib/utils'
 import { OrderStatus, OrderStatusValues } from '@/constants/type'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { DishesDialog } from '@/app/manage/orders/dishes-dialog'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { DishListResType } from '@/schemaValidations/dish.schema'
-
-const fakeOrderDetail = {
-  id: 30,
-  guestId: 70,
-  guest: {
-    id: 70,
-    name: 'An',
-    tableNumber: 2,
-    createdAt: '2024-07-11T04:30:32.728Z',
-    updatedAt: '2024-07-11T05:00:34.131Z'
-  },
-  tableNumber: 2,
-  dishSnapshotId: 36,
-  dishSnapshot: {
-    id: 36,
-    name: 'Spaghetti 5',
-    price: 50000,
-    image: 'http://localhost:4000/static/e0001b7e08604e0dbabf0d8f95e6174a.jpg',
-    description: 'Mỳ ý',
-    status: 'Available',
-    dishId: 2,
-    createdAt: '2024-07-11T04:30:57.450Z',
-    updatedAt: '2024-07-11T04:30:57.450Z'
-  },
-  quantity: 1,
-  orderHandlerId: null,
-  orderHandler: null,
-  status: 'Paid',
-  createdAt: '2024-07-11T04:30:57.450Z',
-  updatedAt: '2024-07-11T04:31:38.806Z',
-  table: {
-    number: 2,
-    capacity: 10,
-    status: 'Reserved',
-    token: '667f3b1ce5e4429990dacea1809d20e7',
-    createdAt: '2024-06-21T06:52:26.847Z',
-    updatedAt: '2024-07-03T04:36:51.130Z'
-  }
-}
+import { useGetOrderDetailQuery, useUpdateOrderMutation } from '@/queries/useOrder'
+import { toast } from 'sonner'
 
 export default function EditOrder({
   id,
@@ -63,8 +26,10 @@ export default function EditOrder({
   setId: (value: number | undefined) => void
   onSubmitSuccess?: () => void
 }) {
-  const [selectedDish, setSelectedDish] = useState<DishListResType['data'][0]>(fakeOrderDetail.dishSnapshot as any)
-  const orderDetail = fakeOrderDetail
+  const [selectedDish, setSelectedDish] = useState<DishListResType['data'][0] | null>(null)
+  const updateOrderMutation = useUpdateOrderMutation()
+  const getOrderDetailQuery = useGetOrderDetailQuery({ id: id as number, enabled: Boolean(id) })
+  const data = getOrderDetailQuery.data
   const form = useForm<UpdateOrderBodyType>({
     resolver: zodResolver(UpdateOrderBody),
     defaultValues: {
@@ -74,10 +39,41 @@ export default function EditOrder({
     }
   })
 
-  const onSubmit = async (values: UpdateOrderBodyType) => {}
+  useEffect(() => {
+    if (data) {
+      const { status, quantity, dishSnapshot } = data.payload.data
+      form.reset({
+        status,
+        quantity,
+        dishId: dishSnapshot.id
+      })
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setSelectedDish(dishSnapshot)
+    }
+  }, [data, form])
+
+  const onSubmit = async (values: UpdateOrderBodyType) => {
+    if (updateOrderMutation.isPending) return
+
+    try {
+      const result = await updateOrderMutation.mutateAsync({
+        orderId: id as number,
+        body: values
+      })
+      toast.success(result.payload.message)
+      reset()
+      onSubmitSuccess?.()
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+  }
 
   const reset = () => {
     setId(undefined)
+    form.reset()
   }
 
   return (
@@ -99,6 +95,7 @@ export default function EditOrder({
             className='grid auto-rows-max items-start gap-4 md:gap-8'
             id='edit-order-form'
             onSubmit={form.handleSubmit(onSubmit, console.log)}
+            onReset={reset}
           >
             <div className='grid gap-4 py-4'>
               <FormField
--- app/manage/orders/order-table.tsx
+++ app/manage/orders/order-table.tsx
@@ -12,13 +12,13 @@ import {
 } from '@tanstack/react-table'
 import { Input } from '@/components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { GetOrdersResType } from '@/schemaValidations/order.schema'
+import { GetOrdersResType, UpdateOrderResType } from '@/schemaValidations/order.schema'
 import AddOrder from '@/app/manage/orders/add-order'
 import EditOrder from '@/app/manage/orders/edit-order'
 import { createContext, useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import AutoPagination from '@/components/auto-pagination'
-import { getVietnameseOrderStatus } from '@/lib/utils'
+import { getVietnameseOrderStatus, handleErrorApi } from '@/lib/utils'
 import { OrderStatusValues } from '@/constants/type'
 import OrderStatics from '@/app/manage/orders/order-statics'
 import orderTableColumns from '@/app/manage/orders/order-table-columns'
@@ -31,8 +31,11 @@ import { Command, CommandGroup, CommandItem, CommandList } from '@/components/ui
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { endOfDay, format, startOfDay } from 'date-fns'
 import { useGetTableList } from '@/queries/useTable'
-import { useOrderListQuery } from '@/queries/useOrder'
+import { useOrderListQuery, useUpdateOrderMutation } from '@/queries/useOrder'
 import TableSkeleton from '@/app/manage/orders/table-skeleton'
+import socket from '@/lib/socket'
+import { toast } from 'sonner'
+import { GuestCreateOrdersResType } from '@/schemaValidations/guest.schema'
 
 export const OrderTableContext = createContext({
   setOrderIdEdit: (value: number | undefined) => {},
@@ -82,6 +85,8 @@ export default function OrderTable() {
     pageSize: PAGE_SIZE //default page size
   })
 
+  const updateOrderMutation = useUpdateOrderMutation()
+
   const { statics, orderObjectByGuestId, servingGuestByTableNumber } = useOrderService(orderList)
 
   const changeStatus = async (body: {
@@ -89,7 +94,18 @@ export default function OrderTable() {
     dishId: number
     status: (typeof OrderStatusValues)[number]
     quantity: number
-  }) => {}
+  }) => {
+    try {
+      await updateOrderMutation.mutateAsync({
+        body,
+        orderId: body.orderId
+      })
+    } catch (error) {
+      handleErrorApi({
+        error
+      })
+    }
+  }
 
   const table = useReactTable({
     data: orderList,
@@ -124,6 +140,57 @@ export default function OrderTable() {
     setFromDate(initFromDate)
     setToDate(initToDate)
   }
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect()
+    }
+
+    function onConnect() {
+      console.log(socket.id)
+    }
+
+    function onDisconnect() {
+      console.log('disconnect')
+    }
+
+    function refetch() {
+      const now = new Date()
+      if (now >= fromDate && now <= toDate) {
+        orderListQuery.refetch()
+      }
+    }
+
+    function onUpdateOrder(data: UpdateOrderResType['data']) {
+      const {
+        dishSnapshot: { name },
+        quantity
+      } = data
+      toast.success(
+        `Món ${name} (SL: ${quantity}) vừa được cập nhật sang trạng thái "${getVietnameseOrderStatus(data.status)}"`
+      )
+      refetch()
+    }
+
+    function onNewOrder(data: GuestCreateOrdersResType['data']) {
+      const { guest } = data[0]
+      toast.success(`${guest?.name} tại bàn ${guest?.tableNumber} vừa đặt ${data.length} đơn`)
+
+      refetch()
+    }
+
+    socket.on('update-order', onUpdateOrder)
+    socket.on('new-order', onNewOrder)
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+      socket.off('update-order', onUpdateOrder)
+      socket.off('new-order', onNewOrder)
+    }
+  }, [orderListQuery, fromDate, toDate])
 
   return (
     <OrderTableContext.Provider
