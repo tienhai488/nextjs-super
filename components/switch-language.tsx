df0293a83eb79f10cda22fca88b8ec379912de29
Le Tien Hai <tienhaile488@gmail.com>
2026-01-20 21:39:12 +0700

    feat: switch languages with i18n routing

--- components/switch-language.tsx
+++ components/switch-language.tsx
@@ -1,17 +1,26 @@
 'use client'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Locale, locales } from '@/config'
-import { setUserLocale } from '@/services/locale'
 import { useLocale, useTranslations } from 'next-intl'
+import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 export function SwitchLanguage() {
   const t = useTranslations('SwitchLanguage')
   const locale = useLocale()
+  const pathname = usePathname()
+  const router = useRouter()
+  const params = useParams()
+  const searchParams = useSearchParams()
+
   return (
     <Select
       value={locale}
       onValueChange={(value) => {
-        setUserLocale(value as Locale)
+        const locale = value as Locale
+        const newPathname = pathname.replace(`/${params.locale}`, `/${locale}`)
+        const fullUrl = `${newPathname}?${searchParams.toString()}`
+        router.replace(fullUrl)
+        router.refresh()
       }}
     >
       <SelectTrigger className='w-[140px]'>
