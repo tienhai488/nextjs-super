d349270618d20edfbc4dd593a85aff29609709b5
Le Tien Hai <tienhaile488@gmail.com>
2026-01-21 21:11:17 +0700

    feat: switch languages with i18n navigation

--- components/switch-language.tsx
+++ components/switch-language.tsx
@@ -3,24 +3,19 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 import { Locale, locales } from '@/config'
 import { usePathname, useRouter } from '@/i18n/navigation'
 import { useLocale, useTranslations } from 'next-intl'
-import { useParams, useSearchParams } from 'next/navigation'
 
 export function SwitchLanguage() {
   const t = useTranslations('SwitchLanguage')
   const locale = useLocale()
   const pathname = usePathname()
   const router = useRouter()
-  const params = useParams()
-  const searchParams = useSearchParams()
 
   return (
     <Select
       value={locale}
       onValueChange={(value) => {
         const locale = value as Locale
-        const newPathname = pathname.replace(`/${params.locale}`, `/${locale}`)
-        const fullUrl = `${newPathname}?${searchParams.toString()}`
-        router.replace(fullUrl)
+        router.replace(pathname, { locale })
         router.refresh()
       }}
     >
