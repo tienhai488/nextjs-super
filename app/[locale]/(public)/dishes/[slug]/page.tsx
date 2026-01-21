d40a1e6ff412130f8286e7ca24a49e6f9424d18a
Le Tien Hai <tienhaile488@gmail.com>
2026-01-21 22:30:31 +0700

    feat: add static params generation for dish detail page

--- app/[locale]/(public)/dishes/[slug]/page.tsx
+++ app/[locale]/(public)/dishes/[slug]/page.tsx
@@ -1,7 +1,16 @@
 import dishApiRequest from '@/apiRequests/dish'
 import DishDetail from '@/app/[locale]/(public)/dishes/[slug]/dish-detail'
-import { extractIdFromSlugUrl, wrapServerApi } from '@/lib/utils'
+import { extractIdFromSlugUrl, generateSlugUrl, wrapServerApi } from '@/lib/utils'
 import { DishResType } from '@/schemaValidations/dish.schema'
+
+export async function generateStaticParams() {
+  const result = await wrapServerApi(() => dishApiRequest.list())
+  const posts = result?.payload?.data || []
+
+  return posts.map((post) => ({
+    slug: generateSlugUrl({ name: post.name, id: post.id })
+  }))
+}
 
 export default async function DishPage({ params }: { params: { slug: string } }) {
   const { slug } = await params
